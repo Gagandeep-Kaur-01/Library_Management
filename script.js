feaf01282224069f1cbd4a366837d817a2f32327
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-06-28 14:41:04 +0530

    added -> READ Request Handlers

---
+++ script.js
@@ -0,0 +1,27 @@
+const express = require('express');
+//const Joi = require('joi'); //used for validation
+const app = express();
+app.use(express.json());
+
+const books = [
+	{title: 'History of Punjab', id: 1},
+	{title: 'Final Asault', id: 2},
+	{title: 'Sher E Punjab', id: 3}
+]
+
+//READ Request Handlers
+app.get('/', (req,res) => {
+	res.send("Welcome to my Library!!!!!!");
+});
+
+app.get('/api/books', (req,res) => {
+	res.send(books);
+});
+
+app.get('/api/books/:id', (req,res) => {
+     const book = books.find(c => c.id == parseInt(req.param.id));
+	 if(!book) res.status(404).send('<h2 style="font-family"Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
+	 res.send(book);
+});
+
+app.listen(8080);
